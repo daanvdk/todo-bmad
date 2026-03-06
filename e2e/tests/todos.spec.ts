cafe4d84b3b11f1f20637a4de7ca75c8adf7bbe3
Daan van der Kallen <daan.vander.kallen@nearform.com>
2026-03-06 10:08:05 +0200

    Add linting to e2e tests

--- e2e/tests/todos.spec.ts
+++ e2e/tests/todos.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from "@playwright/test";
 
-test('app loads without errors', async ({ page }) => {
-  await page.goto('/')
-  await expect(page).not.toHaveTitle('Error')
-  await expect(page.locator('body')).toBeVisible()
-})
+test("app loads without errors", async ({ page }) => {
+  await page.goto("/");
+  await expect(page).not.toHaveTitle("Error");
+  await expect(page.locator("body")).toBeVisible();
+});
